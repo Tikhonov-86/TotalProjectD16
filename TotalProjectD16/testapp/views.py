77c625972d841d08021a0b9568439b7b82df6c3d
Tikhonov-86 <Sashulka-86@list.ru>
2023-12-15 14:04:09 +0500

    Commit 1.111

--- TotalProjectD16/testapp/views.py
+++ TotalProjectD16/testapp/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views import View
 
-# Create your views here.
+
+@permission_required('polls_and_choise', raise_exception=True)
+@login_required
+def my_view(request):
+
+    return HttpResponse(content={'count': count_var})
+
+
+class MyView(LoginRequiredMixin, View):
+    login_url = '/login/'
