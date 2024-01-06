e2b5cd9ceb74e2ef9d416d56f04bdf10d5f35bb8
Tikhonov-86 <Sashulka-86@list.ru>
2024-01-07 02:51:09 +0500

    Commit 1.645

--- TotalProjectD16/testapp/urls.py
+++ TotalProjectD16/testapp/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('<int:pk>/', ArticleDetail.as_view(), name='article_detail'),
     path('<int:pk>/update/', ArticleUpdate.as_view(), name='article_update'),
     path('<int:pk>/delete/', ArticleDelete.as_view(), name='article_delete'),
-    path('<int:pk>/comment/', IndexView.as_view(), name='comment'),
+    path('<int:pk>/comments/', IndexView.as_view(), name='comment'),
     # path('subscriptions/', subscriptions, name='subscriptions'),
 ]
