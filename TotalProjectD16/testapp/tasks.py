2a22dccf55b51d5aafaf95bfa06929fc96b97bcd
Tikhonov-86 <Sashulka-86@list.ru>
2024-04-10 16:42:00 +0500

    Commit 2.29 tasks.py

---
+++ TotalProjectD16/testapp/tasks.py
@@ -0,0 +1,28 @@
+from celery import shared_task
+from django.core.mail import EmailMultiAlternatives
+
+from testapp.models import Comment
+
+
+@shared_task
+def comment_created_task(comment_id):
+    comment = Comment.objects.get(pk=comment_id)
+    email = comment.add.author.user.email
+
+    subject = f'На Ваше объявление оставили отклик'
+
+    text_content = (
+        f'Объявление: {comment.commentPost}\n'
+        f'Описание: {comment.text}\n\n'
+        f'Ссылка на объявление: http://127.0.0.1:8000{comment.get_absolute_url()}'
+    )
+    html_content = (
+        f'Объявление: {comment.commentPost}<br>'
+        f'Описание: {comment.text}<br><br>'
+        f'<a href="http://127.0.0.1/{comment.get_absolute_url()}">'
+        f'Ссылка на объявление</a>'
+    )
+
+    msg = EmailMultiAlternatives(subject, text_content, 'skillfactory@mail.ru', [email])
+    msg.attach_alternative(html_content, "text/html")
+    msg.send()
