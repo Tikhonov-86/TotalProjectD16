9465cd7764a4cdd2ad8d5f53daeff97c18c7d530
Tikhonov-86 <Sashulka-86@list.ru>
2024-04-10 17:06:17 +0500

    Commit 2.30 add def confirm_comment_task in tasks.py

--- TotalProjectD16/testapp/tasks.py
+++ TotalProjectD16/testapp/tasks.py
@@ -1,6 +1,7 @@
 from celery import shared_task
 from django.core.mail import EmailMultiAlternatives
 
+from TotalProjectD16 import settings
 from testapp.models import Comment
 
 
@@ -23,6 +24,51 @@ def comment_created_task(comment_id):
         f'Ссылка на объявление</a>'
     )
 
-    msg = EmailMultiAlternatives(subject, text_content, 'skillfactory@mail.ru', [email])
+    msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [email])
     msg.attach_alternative(html_content, "text/html")
     msg.send()
+
+
+@shared_task
+def confirm_comment_task(comment_id):
+    comment = Comment.objects.get(pk=comment_id)
+    email = comment.user.email
+
+    if comment.status == 'accepted':
+        subject = f'Ваш отклик приняли'
+
+        text_content = (
+            f'Объявление: {comment.commentPost}\n'
+            f'Описание: {comment.text}\n\n'
+            f'Ссылка на объявление: http://127.0.0.1:8000{comment.get_absolute_url()}'
+            f'Принят автором объявления: {comment.add.author.user}'
+        )
+        html_content = (
+            f'Объявление: {comment.commentPost}<br>'
+            f'Описание: {comment.text}<br><br>'
+            f'<a href="http://127.0.0.1/{comment.get_absolute_url()}">'
+            f'Ссылка на объявление</a>'
+            f'Принят автором объявления: {comment.add.author.user}'
+        )
+        msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [email])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
+    elif comment.status == 'rejected':
+        subject = f'Ваш отклик отклонили'
+        text_content = (
+            f'Объявление: {comment.commentPost}\n'
+            f'Описание: {comment.text}\n\n'
+            f'Ссылка на объявление: http://127.0.0.1:8000{comment.get_absolute_url()}'
+            f'Отклонён автором объявления: {comment.add.author.user}'
+        )
+        html_content = (
+            f'Объявление: {comment.commentPost}<br>'
+            f'Описание: {comment.text}<br><br>'
+            f'<a href="http://127.0.0.1/{comment.get_absolute_url()}">'
+            f'Ссылка на объявление</a>'
+            f'Отклонён автором объявления: {comment.add.author.user}'
+        )
+        msg = EmailMultiAlternatives(subject, text_content, settings.DEFAULT_FROM_EMAIL, [email])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
