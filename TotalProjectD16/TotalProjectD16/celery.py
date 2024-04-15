2ea9a8d9b2e4edd7928f78689e00b59dd7254f29
Tikhonov-86 <Sashulka-86@list.ru>
2024-04-15 09:21:39 +0500

    Commit 2.351 Edit celery.py

--- TotalProjectD16/TotalProjectD16/celery.py
+++ TotalProjectD16/TotalProjectD16/celery.py
@@ -1,9 +1,9 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mcdonalds.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testapp.settings')
 
-app = Celery('mcdonalds')
+app = Celery('testapp')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks()
