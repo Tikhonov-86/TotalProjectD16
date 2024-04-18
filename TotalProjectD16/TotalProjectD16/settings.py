759b5a74c5f33297bb362cd5c545d68b79096453
Tikhonov-86 <Sashulka-86@list.ru>
2024-04-18 10:14:44 +0500

    Commit 2.354 Edit settings.py

--- TotalProjectD16/TotalProjectD16/settings.py
+++ TotalProjectD16/TotalProjectD16/settings.py
@@ -156,7 +156,7 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static/'),
 ]
 
 
