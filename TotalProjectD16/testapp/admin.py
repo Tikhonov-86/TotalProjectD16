c2e6d1f9224bbd2efdfaa2612aa2a042a7541301
Tikhonov-86 <Sashulka-86@list.ru>
2024-05-16 11:30:16 +0500

    Commit 3.2 Add youtube in folder ckeditor

--- TotalProjectD16/testapp/admin.py
+++ TotalProjectD16/testapp/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from django.contrib.gis import forms
 
 from testapp.models import Article, User
+
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
 
 admin.site.register(Article)
 admin.site.register(User)
+
+
+class ArticleAdminForm(forms.ModelForm):
+    text = forms.CharField(label="Описание", widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Article
+        fields = '__all__'
--- TotalProjectD16/testapp/models.py
+++ TotalProjectD16/testapp/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User, AbstractUser
-from django.http import HttpResponse
 from django.urls import reverse
 
 
