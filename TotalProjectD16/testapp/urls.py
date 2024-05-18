8314578496f1d2ad206c7a2f92997e1229e4fa5c
Tikhonov-86 <Sashulka-86@list.ru>
2024-05-18 20:48:31 +0500

    Commit 3.22 Add urls.py 'admin_password_reset'

--- TotalProjectD16/testapp/urls.py
+++ TotalProjectD16/testapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 
 from .views import (
     ArticleList, ArticleCreate, ArticleDetail, ArticleUpdate, ArticleDelete,
@@ -18,4 +19,8 @@ urlpatterns = [
     path('confirm/', ConfirmUser.as_view(), name='confirm_user'),
     path('<int:pk>/comment/confirm/', accept_rejected, name='confirm_comment'),  # принять отклик
     path('<int:pk>/comment/reject/', accept_rejected, name='reject_comment'),  # отклонить отклик
+    path('admin/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset',),
+    path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done',),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm',),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete',),
 ]
