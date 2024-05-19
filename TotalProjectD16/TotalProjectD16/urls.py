fe9fda6103fd95bd57a0d8c8e396f9044abfb195
Tikhonov-86 <Sashulka-86@list.ru>
2024-05-19 22:48:54 +0500

    Commit 3.252 Add url.py

--- TotalProjectD16/TotalProjectD16/urls.py
+++ TotalProjectD16/TotalProjectD16/urls.py
@@ -29,4 +29,7 @@ urlpatterns = [
     path('article/', include('testapp.urls'), name='article_list'),
     # path('logout/', LogoutView.as_view)
     path('ckeditor/', include('ckeditor_uploader.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
